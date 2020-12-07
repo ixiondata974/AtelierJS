b48f8c4a6c260f5b7b96a1ab505ecfea2a9e3be3
maodomalick98 <maodomalick.gaye@epsi.fr>
2020-12-07 14:19:29 +0100

    ajout form

--- QRCode/generateQR.js
+++ QRCode/generateQR.js
@@ -1,7 +1,31 @@
 var texte = document.getElementById("texte");
+
+var Formulaire = [];
+var DonneeForm = "";
+/*var Nom = document.getElementById("Nom");
+var Prenom = document.getElementById("Prenom");
+var adresse = document.getElementById("adresse");
+var heure = document.getElementById("heure");*/
 var geneQR = new QRCode(document.getElementById("qrcode"));
 
-function genereQRCode() {
-	var valText = texte.value;
+function genereQRCode(chaine) {
+	var valText = chaine.value;
 	geneQR.makeCode(valText);
 }
+
+
+function DonnesForm()
+{
+	for(var i=0; i<document.forms["QrForm"].length;i++)
+	{
+		Formulaire[i] = document.forms["QrForm"].elements[i].value;
+		DonneeForm += Formulaire[i] + ' ';
+	}
+	genereQRCode(DonneeForm);
+	
+
+}
+	
+
+
+
